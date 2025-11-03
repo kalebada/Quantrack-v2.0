ee767dcc0a102a895f800484ff3bb79c8c705682
Prarthan23 <145659525+Prarthan23@users.noreply.github.com>
2025-11-03 17:04:27 +0530

    navbar minor changes

--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -9,8 +9,8 @@ const NavBar = () => {
         <h1 className='text-white font-[Gued] text-2xl lg:text-3xl'>Quantrack</h1>
         </div>
         <div className='flex items-center gap-4 font-[montserrat] text-sm text-white'>
-            <button className='px-4 py-1 border border-[#9B4DFF] rounded cursor-pointer lg:hover:bg-[#9B4DFF] transition-all duration-200'>Profile</button> 
-            <Link to='/login' className='px-4 py-1 border border-[#9B4DFF] rounded cursor-pointer lg:hover:bg-[#9B4DFF] transition-all duration-200'>Logout</Link> 
+            <button className='px-4 py-1 border border-[#9B4DFF] rounded cursor-pointer lg:hover:bg-[#9B4DFF] transition-all duration-200'>Verify</button> 
+            <Link to='/login' className='px-4 py-1 border border-[#9B4DFF] rounded cursor-pointer lg:hover:bg-[#9B4DFF] transition-all duration-200'>Login</Link> 
         </div>
     </div>
   )
