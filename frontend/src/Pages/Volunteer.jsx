873742aa585c60b873a7f75e830638d442c10945
DilliRani2 <dillirani0305@gmail.com>
2025-12-31 14:53:32 +0530

    Updated volunteer file

--- frontend/src/Pages/Volunteer.jsx
+++ frontend/src/Pages/Volunteer.jsx
@@ -3,18 +3,20 @@ import NavBar from '../Components/NavBar'
 import {QrCode,Plus,ClockAlert,CircleCheck} from 'lucide-react';
 
 const Volunteer = () => {
+    // TEMPORARY (replace after backend integration)
+  const userName = "Volunteer";
   return (
     <div className='h-auto w-full'>
         <NavBar/>
         <div className='min-h-screen w-full bg-zinc-950 flex flex-col justify-start items-start gap-8 md:gap-16 lg:gap-12 px-4 py-4'>
             <div className='w-full flex flex-col lg:flex-row items-start justify-center lg:justify-between lg:items-center gap-4 md:gap-6 lg:gap-4'>
                 <div className='w-full flex flex-col justify-center items-start gap-2 md:gap-4 lg:gap-2'>
-                    <h1 className='font-[montserrat] text-white text-4xl md:text-5xl lg:text-5xl font-bold leading-14'><span className='text-2xl'>Welcome Back,</span><br/>John Doe</h1>
+                    <h1 className='font-[montserrat] text-white text-4xl md:text-5xl lg:text-5xl font-bold leading-14'><span className='text-2xl'>Welcome Back,</span><br/>{userName}</h1>
                     <h2 className='text-gray-400 font-[montserrat] w-[90%] md:text-xl lg:text-base'>Track your engagement and multiply your impact</h2>
                 </div>
                 <div className='w-full flex justify-start lg:justify-end items-center gap-2 lg:gap-4 text-white md:text-xl lg:text-base'>
                     <button className=' bg-[#9B4DFF] px-4 py-2 rounded font-[montserrat] flex justify-center items-center gap-2 cursor-pointer'><QrCode size={22} />Code</button>
-                    <button className=' bg-[#9B4DFF] px-4 py-2 rounded font-[montserrat] flex justify-center items-center gap-2 size={22} cursor-pointer'><Plus />Join Organization</button>
+                    <button className=' bg-[#9B4DFF] px-4 py-2 rounded font-[montserrat] flex justify-center items-center gap-2 cursor-pointer'><Plus size={22}  />Join Organization</button>
                 </div>
             </div>                 
             <div className='w-full flex flex-col justify-start items-start text-white gap-4'>
