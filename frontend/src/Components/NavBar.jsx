aff89ea441b4a80399f5a740fd25955511793b8a
DilliRani2 <dillirani0305@gmail.com>
2025-12-31 17:16:09 +0530

    verify button connected

--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
         <h1 className='text-white font-[Gued] text-2xl lg:text-3xl'>Quantrack</h1>
         </a>
         <div className='flex items-center gap-4 font-[montserrat] text-sm text-white'>
-            <button className='px-4 py-1 border border-[#9B4DFF] rounded cursor-pointer lg:hover:bg-[#9B4DFF] transition-all duration-200'>Verify</button> 
+            <Link to='/verify-email' className='px-4 py-1 border border-[#9B4DFF] rounded cursor-pointer lg:hover:bg-[#9B4DFF] transition-all duration-200'>Verify</Link> 
             <Link to='/login' className='px-4 py-1 border border-[#9B4DFF] rounded cursor-pointer lg:hover:bg-[#9B4DFF] transition-all duration-200'>Login</Link> 
         </div>
     </div>
